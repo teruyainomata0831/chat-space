3204ad79e8d1fca8c8a81c02831a7d6ac9896d94
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-11-13 16:35:18 +0900

    メンターさんによる編集

--- db/migrate/20191108041924_add_sex_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddSexToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :sex, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20191109040440) do
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",                                null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -36,12 +37,8 @@ ActiveRecord::Schema.define(version: 20191109040440) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "introduce"
-    t.integer  "age"
-    t.string   "sex"
-    t.string   "address"
-    t.string   "name",                                null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
