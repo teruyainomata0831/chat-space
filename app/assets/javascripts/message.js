eac3fda8387f6867801cd37dbefc65e42816d6d7
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-11-15 20:29:34 +0900

    chat-space非同期通信終わりました!

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,49 @@
+$(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="main_center_thread_name" data-id="${message.id}">
+                  <div class="main_center_thread_name">
+                    <p class="message__detail__current-thread">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__detail__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message_body">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this);
+    var url = (window.location.href);
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main_center').append(html);
+      $('#message_content').val(''); //input内のメッセージを消しています。
+      $('.main_center').animate({scrollTop: $('.main_center')[0].scrollHeight}, 'swing');
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.form__submit').prop('disabled', false);
+  })
+});
+});
+
