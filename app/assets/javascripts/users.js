0fee15e98ff0c52ada5cbd71836e7ed4d1409627
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-11-17 16:32:19 +0900

    自動更新修正

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -55,7 +55,6 @@ $(function() {
       });
   });
   $(document).on("click", ".chat-group-user__btn--add", function() {
-    console.log
     const userName = $(this).attr("data-user-name");
     const userId = $(this).attr("data-user-id");
     $(this)
@@ -72,27 +71,26 @@ $(function() {
 });
 
 var reloadMessages = function () {
-  if (window.location.href.match(/\/groups\/\d+\/messages/)){//今いるページのリンクが/groups/グループID/messagesのパスとマッチすれば以下を実行。
-    var last_message_id = $('.message:last').data("message-id"); //dataメソッドで.messageにある:last最後のカスタムデータ属性を取得しlast_message_idに代入。
-    // var group_id = $(".group").data("group-id");
+  if (window.location.href.match(/\/groups\/\d+\/messages/)){
+    var last_message_id = $('.message:last').data("message-id");
 
-    $.ajax({ //ajax通信で以下のことを行う
-      url: "api/messages", //サーバを指定。今回はapi/message_controllerに処理を飛ばす
-      type: 'get', //メソッドを指定
-      dataType: 'json', //データはjson形式
-      data: {last_id: last_message_id} //飛ばすデータは先ほど取得したlast_message_id。またparamsとして渡すためlast_idとする。
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {last_id: last_message_id}
     })
-    .done(function (messages) { //通信成功したら、controllerから受け取ったデータ（messages)を引数にとって以下のことを行う
-      var insertHTML = '';//追加するHTMLの入れ物を作る
-      messages.forEach(function (message) {//配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        insertHTML = buildHTML(message); //メッセージが入ったHTMLを取得
-        $('.messages').append(insertHTML);//メッセージを追加
+    .done(function (messages) {
+      var insertHTML = '';
+      messages.forEach(function (message) {
+        insertHTML = buildHTML(message);
+        $('.messages').append(insertHTML);
       })
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');//最新のメッセージが一番下に表示されようにスクロールする。
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
     })
     .fail(function () {
-      alert('自動更新に失敗しました');//ダメだったらアラートを出す
+      alert('自動更新に失敗しました');
     });
-    setInterval(reloadMessages, 7000);//5000ミリ秒ごとにreloadMessagesという関数を実行し自動更新を行う。
+    setInterval(reloadMessages, 7000);
   }
 };
