311550b2b0f2d0a2d87641db85d40e4fc7b18b75
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-11-21 12:53:19 +0900

    message.js変更

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,17 +2,19 @@ $(function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image.url ? `<img src= ${ message.image.url }>` : "";
-    var html = `<div class="main_center_thread_name" data-m-id="${message.id}">
+    var html = `<div class="message" data-message-id="${message.id}">
+                <div class="main_center_thread">
                   <div class="main_center_thread_name">
-                    <p class="message__detail__current-thread">
-                      ${message.user_name}
-                    </p>
-                    <p class="message__detail__date">
-                      ${message.created_at}
-                    </p>
-                  </div>
-                  <p class="message_body">
-                    <div>
+                    ${message.user_name}
+                    </div>
+                  <div class="main_center_thread_date">
+                    ${message.created_at}
+                    </div>
+                    <div class="lower-message">
+                      <p class="lower-message__content">
+                      </p>
+                      </div>
+                    </div>
                     ${content}
                     </div>
                     ${img}
@@ -23,6 +25,7 @@ $(function(){
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       var last_message_id = $('.message:last').data("message-id");
+      console.log(last_message_id);
       $.ajax({
         url: "api/messages",
         type: 'get',
@@ -30,7 +33,6 @@ $(function(){
         data: {last_id: last_message_id}
       })
       .done(function (messages) {
-        console.log(messages)
         var insertHTML = '';
         messages.forEach(function (message) {
           insertHTML = buildHTML(message);
